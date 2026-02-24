9f58bc77ce9f7a9d9d3025071f0e6d466942ac5f
anacarvalho123 <ana_bia_c@hotmail.com>
2026-02-24 11:33:29 +0000

    Implementa funcao para ocupar quarto

--- quartos.py
+++ quartos.py
@@ -9,3 +9,10 @@ quartos = {
 def listar_quartos():
     for numero, status in quartos.items():
         print(f"Quarto {numero} - {status}")
+
+def ocupar_quarto(numero):
+    if numero in quartos:
+        quartos[numero] = "ocupado"
+        print(f"Quarto {numero} agora esta ocupado.")
+    else:
+        print("Quarto nao existe.")

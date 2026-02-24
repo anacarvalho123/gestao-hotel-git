216fd611ec1a43e091bd76704b106582a05ecf67
anacarvalho123 <ana_bia_c@hotmail.com>
2026-02-24 11:40:58 +0000

    Implementa funcao de listagem de reservas

--- reservas.py
+++ reservas.py
@@ -2,3 +2,7 @@ reservas = []
 
 def criar_reserva(cliente, quarto):
     reservas.append((cliente, quarto))
+
+def listar_reservas():
+    for cliente, quarto in reservas:
+        print(f"Cliente: {cliente} - Quarto: {quarto}")
